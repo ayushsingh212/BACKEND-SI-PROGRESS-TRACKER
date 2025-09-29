0d7da615f9ec81173154bdf1cb5992ae6e7f5879
ayushsingh212 <ayurajput249@gmail.com>
2025-09-29 23:39:01 +0530

    Fixed the import of custom apiError

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,7 +2,7 @@ import { options } from "../constants.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
 
 const generateAccessAndRefreshToken = async (userId) => {
   const user = await User.findById(userId);
