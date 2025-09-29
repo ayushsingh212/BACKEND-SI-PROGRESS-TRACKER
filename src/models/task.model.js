9f60aa898389dba2b4b1ee48fa1eeb6ad23dd388
ayushsingh212 <ayurajput249@gmail.com>
2025-09-29 21:36:26 +0530

    Added the user and task model with some additional utility function

---
+++ src/models/task.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+
+
+const taskSchema = new mongoose.Schema({
+  
+  userId:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+  },
+  taskName:{
+   type:String,
+   lowercase:true,
+  trim:true,
+  required:true,
+
+  },
+  taskDetails:{
+    type:String,
+    required:[true,"Task details is required to let you know what to do!"],
+
+  },
+  taskStatus:{
+    type:String,
+  enum: ["started", "in-progress", "completed"],
+  default:"started"
+  }
+
+
+},{
+  timestamps:true
+});
+
+
+export const Task = mongoose.model("Task",taskSchema);
