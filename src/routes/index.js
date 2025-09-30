a3360d0fa90cfaf308f68186afe64e5a13a43eca
ayushsingh212 <ayurajput249@gmail.com>
2025-09-30 13:52:14 +0530

    Created the task Routes and connected them with express

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import userRoutes from "./user.routes.js";
-
+import taskRoutes from "./task.routes.js"
 import { Router } from "express";
 
 const router = Router();
 
 
 router.use("/user",userRoutes);
+router.use("/task",taskRoutes)
 
 
 
---
+++ src/routes/task.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { createTask, getTaskStatus, updateTask, updateTaskStatus } from "../controllers/task.controllers.js";
+
+
+
+
+
+
+
+const router = Router();
+
+
+
+
+
+router.use(verifyJWT);
+
+router.post("/createTask",createTask);
+router.put("/updateTask/:taskId",updateTask);
+router.patch("/updateTaskStatus/:taskId",updateTaskStatus);
+router.get("/getTaskStatus/:taskId",getTaskStatus);
+
+
+
+export default router;
